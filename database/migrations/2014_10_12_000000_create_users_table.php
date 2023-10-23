c3324575136b5debfd7b18ddcdbad48b5ac78d70
GG <mohdhaziziak@gmail.com>
2023-10-23 15:32:19 +0800

    run build, pa migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -25,7 +25,7 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
-        User::create(['name' => 'admin','email' => 'admin@themesbrand.com','password' => Hash::make('12345678'),'email_verified_at'=>'2022-01-02 17:04:58','avatar' => 'avatar-1.jpg','created_at' => now(),]);
+        User::create(['name' => 'admin','email' => 'mohdhaziziak@gmail.com','password' => Hash::make('12345678'),'email_verified_at'=>'2023-01-01 00:00:00','avatar' => 'avatar-1.jpg','created_at' => now(),]);
     }
     /**
      * Reverse the migrations.
---
+++ storage/framework/views/00c6e479ae0ddba488fa5d9b867c9429.php
@@ -0,0 +1,5 @@
+
+<body>
+
+<!-- <body data-layout="horizontal"> -->
+<?php /**PATH C:\Users\hazizi-3TDS\Desktop\CODE\mpm-muet\resources\views/layouts/body.blade.php ENDPATH**/ ?>
