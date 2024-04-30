14201814134587ed61432be55f4dd5f3b1321df4
luqzi99 <mluqmanhakim.mlh@gmail.com>
2024-05-01 00:28:49 +0800

    create table payment

---
+++ database/migrations/2024_04_30_160555_create_payments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('order_id');
+            $table->timestamp('payment_date');
+            $table->string('method');
+            $table->string('channel');
+            $table->float('amount');
+            $table->string('currency');
+            $table->string('status');
+            $table->string('transaction_id');
+            $table->string('references_number');
+            $table->string('error_message');
+            $table->string('cust_info');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
