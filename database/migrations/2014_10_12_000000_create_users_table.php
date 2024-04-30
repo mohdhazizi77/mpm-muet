bf71e4812d084eb5153f1341f43a7c1422e57f26
luqzi99 <mluqmanhakim.mlh@gmail.com>
2024-04-30 23:13:23 +0800

    update user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,68 +20,14 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('identity_card_number');
-            $table->string('phone_num');
-            $table->text('address');
-            $table->text('avatar');
+            $table->string('identity_card_number')->nullable();
+            $table->string('phone_num')->nullable();
+            $table->text('address')->nullable();
+            $table->text('avatar')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
-        User::create([
-            'name' => 'Mohd Hazizi',
-            'email' => 'pentadbirmuet@gmail.com',
-            'password' => Hash::make('123456'),
-            'identity_card_number' => '',
-            'email_verified_at' => '2023-01-01 00:00:00',
-            'avatar' => 'avatar-1.jpg',
-            'created_at' => now(),
-        ]);
-        User::create([
-            'name' => 'Mohd Hazizi',
-            'email' => 'calonmuet@gmail.com',
-            'password' => Hash::make('000000000000'),
-            'identity_card_number' => '000000000000',
-            'email_verified_at' => '2023-01-01 00:00:00',
-            'avatar' => 'avatar-1.jpg',
-            'created_at' => now(),
-        ]);
-        User::create([
-            'name' => 'MOD-ADMIN',
-            'email' => 'modadmin@gmail.com',
-            'password' => Hash::make('123456'),
-            'identity_card_number' => '',
-            'email_verified_at' => '2023-01-01 00:00:00',
-            'avatar' => 'avatar-1.jpg',
-            'created_at' => now(),
-        ]);
-        User::create([
-            'name' => 'MUET-ADMIN',
-            'email' => 'muetadmin@gmail.com',
-            'password' => Hash::make('123456'),
-            'identity_card_number' => '',
-            'email_verified_at' => '2023-01-01 00:00:00',
-            'avatar' => 'avatar-1.jpg',
-            'created_at' => now(),
-        ]);
-        User::create([
-            'name' => 'BPCOM-ADMIN',
-            'email' => 'bpcomadmin@gmail.com',
-            'password' => Hash::make('123456'),
-            'identity_card_number' => '',
-            'email_verified_at' => '2023-01-01 00:00:00',
-            'avatar' => 'avatar-1.jpg',
-            'created_at' => now(),
-        ]);
-        User::create([
-            'name' => 'PSM-ADMIN',
-            'email' => 'psmadmin@gmail.com',
-            'password' => Hash::make('123456'),
-            'identity_card_number' => 'psmadmin',
-            'email_verified_at' => '2023-01-01 00:00:00',
-            'avatar' => 'avatar-1.jpg',
-            'created_at' => now(),
-        ]);
 
     }
 
