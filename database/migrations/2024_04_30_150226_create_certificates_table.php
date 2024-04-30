3674340467d127838bdf16ce1d550e9634444128
luqzi99 <mluqmanhakim.mlh@gmail.com>
2024-05-01 00:26:40 +0800

    create new table Certificate

---
+++ database/migrations/2024_04_30_150226_create_certificates_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('certificates', function (Blueprint $table) {
+            $table->id();
+            $table->string('cid')->nullable();
+            $table->integer('candidate_id');
+            $table->integer('exam_session_id');
+            $table->text('result');
+            $table->timestamp('issue_date')->nullable();
+            $table->timestamp('expire_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('certificates');
+    }
+};
