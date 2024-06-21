b1b96b9317989caa666c9c08a873c2a8197482e7
luqzi99 <mluqmanhakim.mlh@gmail.com>
2024-06-21 10:09:46 +0800

    update return page

--- resources/views/modules/candidates/print-mpm-return.blade.php
+++ resources/views/modules/candidates/print-mpm-return.blade.php
@@ -21,7 +21,7 @@
                                     <table class="table-borderless fs-16 mt-3">
                                         <tr>
                                             <td class="fw-bold">
-                                                TRANSACTION REFERENCE : {{ $txn_id }}
+                                                REFERENCE ID: {{ $order->unique_order_id }}
                                             </td>
                                         </tr>
                                         <tr>
@@ -33,11 +33,7 @@
                                     </table>
                                 </div>
                             </div>
-                            {{--                        <div class="col-xxl-3 ms-auto">--}}
-                            {{--                            <div class="mb-n5 pb-1 faq-img d-none d-xxl-block">--}}
-                            {{--                                <img src="{{ URL::asset('build/images/faq-img.png') }}" alt="" class="img-fluid">--}}
-                            {{--                            </div>--}}
-                            {{--                        </div>--}}
+
                         </div>
                     </div>
                     <!-- end card body -->
@@ -51,8 +47,7 @@
                             <div class="row">
                                 <div class="col-xxl-12 align-self-center">
                                     <div class="py-4">
-                                        {{--                                <h2 class="display-8 coming-soon-text text-success">TEST LIST</h2>--}}
-                                        <!-- Striped Rows -->
+
                                         <table class="table table-striped text-center">
                                             <thead>
                                             <tr class="text-center bg-dark-subtle">
@@ -65,92 +60,15 @@
                                             <tbody>
                                             <tr class="align-middle">
                                                 <th scope="row">1</th>
-                                                <td>09/12/2023 12:30:00</td>
-                                                <td></td>
-                                                <td><h5><span class="badge rounded-pill bg-info">PROCESSING</span></h5></td>
+                                                <td>{{ $order->created_at->format('d/m/y H:i:s') }}</td>
+                                                <td>User had  make payment</td>
+                                                <td><h5><span class="badge rounded-pill bg-info">{{ $order->current_status }}</span></h5></td>
                                             </tr>
                                             </tbody>
                                         </table>
-
-                                        {{--MODAL VERIFY PDF--}}
-                                        <div id="modalVerifyPDF" class="modal fade" tabindex="-1" aria-hidden="true" style="display: none;">
-                                            <div class="modal-dialog modal-dialog-centered">
-                                                <div class="modal-content border-0 overflow-hidden">
-                                                    <div class="modal-header p-3 bg-dark-subtle">
-                                                        <h4 class="card-title mb-0">Index Number Verification</h4>
-                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                    </div>
-                                                    <div class="modal-body">
-                                                        <form>
-                                                            <div class="mb-5">
-                                                                <label for="indexNumber" class="form-label">Index Number</label>
-                                                                <input type="text" class="form-control mb-2" id="indexNumber">
-                                                                <a class="text-decoration-none text-black-50" href="#">Forgot Index Number?</a>
-                                                            </div>
-                                                            <div class="clearfix">
-                                                                <button type="button" class="btn btn-soft-dark waves-effect waves-light" data-bs-dismiss="modal">Cancel</button>
-                                                                <a href="{{ Route('candidate.printpdf', ['id' => 41]) }}" class="btn btn-soft-success waves-effect waves-light w-md float-end" data-text="Verify"><span>Verify</span></a>
-                                                            </div>
-                                                        </form>
-                                                    </div>
-                                                </div><!-- /.modal-content -->
-                                            </div><!-- /.modal-dialog -->
-                                        </div><!-- /.modal -->
-
-                                        {{--MODAL VERIFY MPMF--}}
-                                        <div id="modalVerifyMPM" class="modal fade" tabindex="-1" aria-hidden="true" style="display: none;">
-                                            <div class="modal-dialog modal-dialog-centered">
-                                                <div class="modal-content border-0 overflow-hidden">
-                                                    <div class="modal-header p-3 bg-dark-subtle">
-                                                        <h4 class="card-title mb-0">Index Number Verification</h4>
-                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                    </div>
-                                                    <div class="modal-body">
-                                                        <form>
-                                                            <div class="mb-5">
-                                                                <label for="indexNumber" class="form-label">Index Number</label>
-                                                                <input type="text" class="form-control mb-2" id="indexNumber">
-                                                                <a class="text-decoration-none text-black-50" href="#">Forgot Index Number?</a>
-                                                            </div>
-                                                            <div class="clearfix">
-                                                                <button type="button" class="btn btn-soft-dark waves-effect waves-light" data-bs-dismiss="modal">Cancel</button>
-                                                                <a href="{{ Route('candidate.printmpm', ['id' => 41]) }}" class="btn btn-success btn-animation waves-effect waves-light w-md float-end" data-text="Verify"><span>Verify</span></a>
-                                                            </div>
-                                                        </form>
-                                                    </div>
-                                                </div><!-- /.modal-content -->
-                                            </div><!-- /.modal-dialog -->
-                                        </div><!-- /.modal -->
-
-                                        {{--MODAL PAYMENT--}}
-                                        <div id="modalPayment" class="modal fade" tabindex="-1" aria-hidden="true" style="display: none;">
-                                            <div class="modal-dialog modal-dialog-centered">
-                                                <div class="modal-content border-0 overflow-hidden">
-                                                    <div class="modal-header p-3 bg-dark-subtle">
-                                                        <h4 class="card-title mb-0">Payment</h4>
-                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                    </div>
-                                                    <div class="modal-body">
-                                                        <form>
-                                                            <div class="mb-3">
-                                                                <label class="form-label">Candidate must pay RM20 to print MUET certificate</label>
-                                                            </div>
-                                                            <div class="text-end">
-                                                                <a href="#" class="btn btn-success btn-animation waves-effect waves-light" data-text="Continue"><span>Continue</span></a>
-                                                            </div>
-                                                        </form>
-                                                    </div>
-                                                </div><!-- /.modal-content -->
-                                            </div><!-- /.modal-dialog -->
-                                        </div><!-- /.modal -->
-
                                     </div>
                                 </div>
-                                {{--                        <div class="col-xxl-3 ms-auto">--}}
-                                {{--                            <div class="mb-n5 pb-1 faq-img d-none d-xxl-block">--}}
-                                {{--                                <img src="{{ asset('build/images/faq-img.png') }}" alt="" class="img-fluid">--}}
-                                {{--                            </div>--}}
-                                {{--                        </div>--}}
+
                             </div>
                         </div>
                         <!-- end card body -->
