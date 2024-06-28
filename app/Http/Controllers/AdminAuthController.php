9a40c9b2b84f35774384b9cf97c11fedd2b24261
luqzi99 <mluqmanhakim.mlh@gmail.com>
2024-06-28 23:55:55 +0800

    update

--- app/Http/Controllers/AdminAuthController.php
+++ app/Http/Controllers/AdminAuthController.php
@@ -21,14 +21,17 @@ class AdminAuthController extends Controller
         ]);
 
         $user = User::where('email', $request->email)->first();
-        // if($user->is_deleted == 1){
-        //     // return response('Your Account in Inactive, Please contact system admin', 422);
-        //     return redirect()->back()->with('fail', 'Your Account is Inactive, Please contact system admin');
-        // }
 
-        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
-            return redirect(route('admin.index'));
+        if($user){
+            if($user->is_deleted == 1){
+                // return response('Your Account in Inactive, Please contact system admin', 422);
+                return redirect()->back()->with('fail', 'Your Account is Inactive, Please contact system admin');
+            }
+            if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+                return redirect(route('admin.index'));
+            }
         }
+
 
         return back()->withInput($request->only('email'))->withErrors(['email' => 'Invalid credentials']);
     }
