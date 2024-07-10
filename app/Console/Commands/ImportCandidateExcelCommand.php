e71549c74321cfe1e3004f44896364f6e08d94ae
luqzi99 <mluqmanhakim.mlh@gmail.com>
2024-07-10 13:14:21 +0800

    new import file

--- app/Console/Commands/ImportCandidateExcelCommand.php
+++ app/Console/Commands/ImportCandidateExcelCommand.php
@@ -36,11 +36,20 @@ class ImportCandidateExcelCommand extends Command
         Excel::import(new ImportMuetCandidate, resource_path('excel/importExcelFiles/MUET.xlsx'));
         Excel::import(new ImportModCandidate, resource_path('excel/importExcelFiles/MOD.xlsx'));
 
-        for ($i=1; $i < 10; $i++) {
+        $arr = [
+            2022,2023,2024
+        ];
+
+        foreach ($arr as $key => $year) {
+            Excel::import(new ImportMuetCandidate, resource_path('excel/importExcelFiles/Data Sijil MUET/data sijil MUET '.$year.'.csv'));
+        }
+
+        // for ($i=1; $i < 10; $i++) {
         // for ($i=10; $i < 20; $i++) {
         // for ($i=20; $i < 33; $i++) {
-            Excel::import(new ImportMuetCandidate, resource_path('excel/importExcelFiles/MUET_2022-2023_chunks/MUET 2022-2023('.$i.').csv'));
-        }
+            // Excel::import(new ImportMuetCandidate, resource_path('excel/importExcelFiles/MUET_2022-2023_chunks/MUET 2022-2023('.$i.').csv'));
+        // }
+
         // Excel::import(new ImportMuetCandidate, resource_path('excel/importExcelFiles/First_10000.csv'));
         // Excel::import(new ImportMuetCandidate, resource_path('excel/importExcelFiles/MUET 2022-2023.xlsx'));
 
