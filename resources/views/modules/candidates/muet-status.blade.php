028d790fa03ab3612b4d696f70fc16f0a2f956e5
luqzi99 <mluqmanhakim.mlh@gmail.com>
2024-08-24 00:11:02 +0800

    update back button

--- resources/views/modules/candidates/muet-status.blade.php
+++ resources/views/modules/candidates/muet-status.blade.php
@@ -146,7 +146,7 @@
         @endif
 
         <div>
-            <a href="javascript:history.go(-1)" class="btn btn-soft-dark btn-label btn-border btn-outline-dark waves-effect waves-light w-lg float-start">
+            <a href="{{ url()->previous() }}" class="btn btn-soft-dark btn-label btn-border btn-outline-dark waves-effect waves-light w-lg float-start">
                 <i class="ri-reply-fill label-icon align-middle fs-16 me-2"></i>BACK TO ORDER HISTORY
             </a>
         </div>
