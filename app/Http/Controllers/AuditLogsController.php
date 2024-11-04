d6974fcc397243d0445815f295b3609be93bcd9c
luqzi99 <mluqmanhakim.mlh@gmail.com>
2024-11-05 00:11:16 +0800

    pagination auditlog

--- app/Http/Controllers/AuditLogsController.php
+++ app/Http/Controllers/AuditLogsController.php
@@ -21,27 +21,59 @@ class AuditLogsController extends Controller
 
     }
 
-    public function getAjax(){
+    // public function getAjax(){
 
-        $logs = AuditLog::latest()->get();
+    //     $logs = AuditLog::latest()->get();
+
+    //     $data = [];
+    //     foreach ($logs as $key => $log) {
+    //         $data[$key] = [
+    //             'action'    => $log->activity,
+    //             'created_date' => $log->created_at->format('d/m/y H:i:s'),
+    //             'data'      => unserialize($log->summary),
+    //         ];
+    //         if (!empty($log->user->id)) {
+    //             $data[$key]['user_id'] = $log->user->id;
+    //             $data[$key]['user_name'] = $log->user->name;
+    //         } else {
+    //             $data[$key]['user_id'] = $log->candidate->id;
+    //             $data[$key]['user_name'] = $log->candidate->name;
+    //         }
+
+    //     }
+
+    //     return datatables($data)->toJson();
+    // }
+
+    public function getAjax()
+    {
+        // Retrieve only 100 records per page using pagination
+        $logs = AuditLog::latest()->paginate(100);
 
         $data = [];
         foreach ($logs as $key => $log) {
             $data[$key] = [
-                'action'    => $log->activity,
-                'created_date' => $log->created_at->format('d/m/y H:i:s'),
-                'data'      => unserialize($log->summary),
+                'action'        => $log->activity,
+                'created_date'  => $log->created_at->format('d/m/y H:i:s'),
+                'data'          => unserialize($log->summary),
             ];
+
             if (!empty($log->user->id)) {
-                $data[$key]['user_id'] = $log->user->id;
+                $data[$key]['user_id']   = $log->user->id;
                 $data[$key]['user_name'] = $log->user->name;
             } else {
-                $data[$key]['user_id'] = $log->candidate->id;
+                $data[$key]['user_id']   = $log->candidate->id;
                 $data[$key]['user_name'] = $log->candidate->name;
             }
-
         }
 
-        return datatables($data)->toJson();
+        // Use Datatables with pagination
+        return datatables()->of($data)->with('pagination', [
+            'total' => $logs->total(),
+            'per_page' => $logs->perPage(),
+            'current_page' => $logs->currentPage(),
+            'last_page' => $logs->lastPage(),
+        ])->toJson();
     }
+
 }
