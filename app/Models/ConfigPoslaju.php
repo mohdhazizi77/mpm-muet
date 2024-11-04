faa97e15b789e7bcac726487e42542d4ecfaa249
luqzi99 <mluqmanhakim.mlh@gmail.com>
2024-11-04 23:57:03 +0800

    update berare token

--- app/Models/ConfigPoslaju.php
+++ app/Models/ConfigPoslaju.php
@@ -89,7 +89,7 @@ class ConfigPoslaju extends Model
             curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
 
             $output = curl_exec($curl);
-            return $output;
+            // return $output;
             if (curl_errno($curl)) {
                 error_log("cURL error: " . curl_error($curl));
                 curl_close($curl);
