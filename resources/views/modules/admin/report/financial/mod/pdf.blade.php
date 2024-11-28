63a9c0bb663930688e7eb538f94207ef70bb433e
luqzi99 <mluqmanhakim.mlh@gmail.com>
2024-11-28 22:34:43 +0800

    change kewangan to transaksi

--- resources/views/modules/admin/report/financial/mod/pdf.blade.php
+++ resources/views/modules/admin/report/financial/mod/pdf.blade.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Senarai Kewangan Mod</title>
+    <title>Senarai Transaksi Mod</title>
 </head>
 <body>
     <div>
         <div style="text-align: center;padding-bottom: 10px">
-            <label for="" style="font-size: 18pt;font-weight: bolder">Senarai Kewangan Mod</label>
+            <label for="" style="font-size: 18pt;font-weight: bolder">Senarai Transaksi Mod</label>
         </div>
         <table width="100%" style="border-collapse: collapse">
             <thead>
--- resources/views/modules/admin/report/financial/muet/pdf.blade.php
+++ resources/views/modules/admin/report/financial/muet/pdf.blade.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Senarai Kewangan Muet</title>
+    <title>Senarai Transaksi Muet</title>
 </head>
 <body>
     <div>
         <div style="text-align: center;padding-bottom: 10px">
-            <label for="" style="font-size: 18pt;font-weight: bolder">Senarai Kewangan Muet</label>
+            <label for="" style="font-size: 18pt;font-weight: bolder">Senarai Transaksi Muet</label>
         </div>
         <table width="100%" style="border-collapse: collapse">
             <thead>
